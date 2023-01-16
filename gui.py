39a6e784fed92a9af9e811204d1b8d7c567e3fbf
M4yfl0w3r <mr.mayflower15@gmail.com>
2023-01-16 18:43:21 +0100

    Sync

--- gui.py
+++ gui.py
@@ -1,9 +1,11 @@
 import PySimpleGUI as gui
+import tensorflow  as tf
+import numpy
 
-from io         import BytesIO
-from pathlib    import Path
-from PIL        import Image, UnidentifiedImageError
-
+from io            import BytesIO
+from pathlib       import Path
+from PIL           import Image, UnidentifiedImageError
+from keras.models  import load_model
 
 def image_to_data(image):
     with BytesIO() as output:
@@ -11,16 +13,36 @@ def image_to_data(image):
         data = output.getvalue()
     return data
 
+class NeuralNetModel:
+
+    def __init__(self):
+        self.model = load_model('CancerDetectionModel.h5')
+
+    def process_image(self, imagePath):
+        img        = tf.keras.utils.load_img(imagePath, target_size=(128, 128))
+        img_tensor = tf.keras.utils.img_to_array(img)                  
+        img_tensor = numpy.expand_dims(img_tensor, axis=0)        
+        img_tensor = img_tensor / 255.                    
+
+        return img_tensor
+
+    def predict(self, imagePath):
+        image = self.process_image(imagePath)
+        prediction = self.model.predict(image)
+        print(prediction)
+
 class Gui:
     
     def __init__(self):
         gui.theme('DarkBlue')
-
-        self.size = 640, 480
+        self.width  = 640
+        self.height = 480
+        self.size   = self.width, self.height
 
         self.layout = [[gui.Text('Choose image file')],
-                       [gui.Input(expand_x = True, key = 'FilePath'), gui.Button('Browse')],
+                       [gui.Input(expand_x = True, disabled = True, key = 'FilePath'), gui.Button('Browse')],
                        [gui.Text('', expand_x = True, key = 'Status')],
+                       [gui.Input(expand_x = True, disabled = True, key = 'Result'), gui.Button('Classify')],
                        [gui.Image(self.size, key = 'Image')]]
 
         self.window = gui.Window('Cancer detection', self.layout)
@@ -48,14 +70,12 @@ class Gui:
                 try:
                     image = Image.open(filePath)
                 except UnidentifiedImageError:
-                    self.window['Status'].update('Cannot identify image file!')
+                    self.window['Status'].update('This file type is not supported!')
                     continue
 
-                imageWidth, imageHeight = image.size
-                scale = min(640 / imageWidth, 480 / imageHeight, 1)
-                
-                if scale != 1:
-                    image = image.resize((int(imageWidth * scale), int(imageHeight * scale)))
-                
-                data = image_to_data(image)
-                self.window['Image'].update(data = data, size = self.size)
+                dataImage = image_to_data(image)
+                self.window['Image'].update(data = dataImage, size = self.size)
+
+            if event == 'Classify':
+                model = NeuralNetModel()
+                self.window['Result'].update(model.predict(filePath))
